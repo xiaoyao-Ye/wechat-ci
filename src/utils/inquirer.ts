7da9ce6de156f16ddbbda7f7673a0f4fcaadc1e8
叶聪 <yqcyecong@163.com>
2022-08-08 18:20:33 +0800

    feat: 🎸 add inquirer

--- src/utils/inquirer.ts
+++ src/utils/inquirer.ts
@@ -1,12 +1,29 @@
 import inquirer from 'inquirer'
+interface Options {
+  mode: 'preview' | 'upload'
+  version: string
+  desc?: string
+}
+export const useInquirer = async (): Promise<Options> => {
+  const descRule = [{ name: 'desc', message: '请输入版本描述:', validate: (desc: string) => desc.length > 0 }]
+  const rule = [
+    { name: 'mode', message: '预览还是上传:', type: 'list', default: 'preview', choices: ['preview', 'upload'] },
+    {
+      name: 'version',
+      message: '请输入当前版本号:',
+      default: '0.0.1',
+      validate: (version: string) => {
+        return /^\d+\.\d+\.\d+$/.test(version)
+      },
+    },
+  ]
 
-export const useInquirer = async () => {
-  const rule = [{}]
-  // 版本号
-  //   获取当前版本号
-  //   根据当前版本号进行patch, minor, major
-  // 项目备注
-  // 开发者
-  // 当前是预览还是发布
-  const {} = await inquirer.prompt(rule)
+  const { mode, version } = await inquirer.prompt(rule)
+
+  if (mode === 'upload') {
+    const { desc } = await inquirer.prompt(descRule)
+    return { mode, desc, version }
+  }
+
+  return { mode, version }
 }
